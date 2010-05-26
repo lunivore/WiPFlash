5177045e48729a2d87be3a8282563e03a0c4afa1
liz <liz@lunivorevm.cable.virginmedia.net>
2010-05-26 22:09:13 +0100

    Refactored scenario to be more BDD-esque

---
+++ Scenarios/PetShopSteps.cs
@@ -0,0 +1,28 @@
+using System;
+using WiPFlash;
+using WiPFlash.Components;
+
+namespace Scenarios
+{
+    public class PetShopSteps
+    {
+        public const string EXAMPLE_APP_PATH =
+            @"..\..\..\" + EXAMPLE_APP_NAME + @"\bin\debug\" + EXAMPLE_APP_NAME + ".exe";
+
+        public const string EXAMPLE_APP_NAME = "ExampleUIs";
+        public const string EXAMPLE_APP_WINDOW_NAME = "petShopWindow";
+
+        private readonly Universe _universe;
+
+        public PetShopSteps(Universe universe)
+        {
+            _universe = universe;
+        }
+
+        public void IsRunning()
+        {
+            Application application = new ApplicationLauncher(TimeSpan.Parse("00:00:20")).LaunchOrRecycle(EXAMPLE_APP_NAME, EXAMPLE_APP_PATH);
+            _universe.Window = application.FindWindow(EXAMPLE_APP_WINDOW_NAME);
+        }
+    }
+}
