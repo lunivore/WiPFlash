39fad95f1d09fca37412c8601232695d15377961
Liz Keogh <lunivore@users.noreply.github.com>
2010-09-22 18:55:23 +0100

    Added methods for searching for windows by condition and with a handler

--- Examples/Component/ApplicationBehaviour.cs
+++ Examples/Component/ApplicationBehaviour.cs
@@ -1,10 +1,12 @@
 ﻿#region
 
 using System;
+using System.Windows.Automation;
 using NUnit.Framework;
 using WiPFlash.Components;
 using WiPFlash.Examples.ExampleUtils;
 using WiPFlash.Exceptions;
+using WiPFlash.Framework;
 
 #endregion
 
@@ -18,6 +20,15 @@ namespace WiPFlash.Examples.Component
         {
             Application application = new ApplicationLauncher().LaunchOrRecycle(EXAMPLE_APP_NAME, EXAMPLE_APP_PATH, Assert.Fail);
             Window window = application.FindWindow("petShopWindow");
+            Assert.IsNotNull(window.Element);
+        }
+
+        [Test]
+        public void ShouldFindWindowByCondition()
+        {
+            Application application = new ApplicationLauncher().LaunchOrRecycle(EXAMPLE_APP_NAME, EXAMPLE_APP_PATH, Assert.Fail);
+            Window window = application.FindWindow(new AndCondition(
+                FindBy.WpfName("petShopWindow"), FindBy.ControlType(ControlType.Window)));
             Assert.IsNotNull(window.Element);
         }
 
@@ -49,6 +60,40 @@ namespace WiPFlash.Examples.Component
             var timeBetween = finished.Subtract(started);
 
             Assert.LessOrEqual(timeBetween, longEnough);
+        }
+
+        [Test]
+        public void ShouldBeAbleToTellWhetherAWindowExistsOrNotWithinTheSpecifiedAmountOfTime()
+        {
+            TimeSpan farFarTooLong = TimeSpan.Parse("00:01:00");
+            TimeSpan longEnough = TimeSpan.Parse("00:00:01");
+            TimeSpan aBitExtraForProcessing = TimeSpan.Parse("00:00:01");
+
+            var started = DateTime.Now;
+
+            Application application = new ApplicationLauncher(farFarTooLong).LaunchOrRecycle(EXAMPLE_APP_NAME, EXAMPLE_APP_PATH, Assert.Fail);
+
+            bool windowDoesNotExist = false;
+            application.FindWindow("Wibble", longEnough, (message) => windowDoesNotExist = true);
+            var finished = DateTime.Now;
+            var timeBetween = finished.Subtract(started);
+
+            Assert.IsTrue(windowDoesNotExist);
+            Assert.LessOrEqual(timeBetween, longEnough.Add(aBitExtraForProcessing));
+
+            windowDoesNotExist = false;
+            started = DateTime.Now;
+            application.FindWindow(new AndCondition(new PropertyCondition(AutomationElement.NameProperty, "Wibble"),
+                                                    new PropertyCondition(AutomationElement.ControlTypeProperty,
+                                                                          ControlType.Window)),
+                                                                          longEnough,
+                                                                          message => windowDoesNotExist = true);
+            finished = DateTime.Now;
+            timeBetween = finished.Subtract(started);
+
+            Assert.IsTrue(windowDoesNotExist);
+            Assert.LessOrEqual(timeBetween, longEnough.Add(aBitExtraForProcessing));
+
         }
     }
 }
