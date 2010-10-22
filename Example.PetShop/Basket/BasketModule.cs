18b1add96b7dfadf240fa4c242764b4dba3d236c
Liz Keogh <lunivore@users.noreply.github.com>
2010-10-22 16:15:58 +0100

    Oh, so *that's* how you use the Container...

--- Example.PetShop/Basket/BasketModule.cs
+++ Example.PetShop/Basket/BasketModule.cs
@@ -14,21 +14,23 @@ namespace Example.PetShop.Basket
     public class BasketModule : IModule
     {
         private readonly IUnityContainer _container;
+        private readonly PetRepository _petRepository;
+        private readonly AccessoryRepository _accessoryRepository;
         private readonly IRegionManager _regionManager;
 
-        public BasketModule(IRegionManager regionManager, IUnityContainer container)
+        public BasketModule(IRegionManager regionManager, IUnityContainer container, PetRepository petRepository, AccessoryRepository accessoryRepository)
         {
             _regionManager = regionManager;
             _container = container;
+            _petRepository = petRepository;
+            _accessoryRepository = accessoryRepository;
         }
 
         #region IModule Members
 
         public void Initialize()
         {
-            var petRepository = _container.Resolve<PetRepository>();
-            var accessoryRepository = _container.Resolve<AccessoryRepository>();
-            var basketViewModel = new BasketViewModel(petRepository, accessoryRepository);
+            var basketViewModel = new BasketViewModel(_petRepository, _accessoryRepository);
             var panel = new BasketPanel(basketViewModel);
             _regionManager.Regions["Sales"].Add(panel);
             _regionManager.Regions["Sales"].Activate(panel);
